429968112368442a44efc9fbe444502f5577baeb
Mary Webby <MaryWebby@Marys-MacBook-Air-3.local>
2023-01-04 20:53:31 -0500

    created files with html setup and style css

---
+++ assets/script.js
@@ -0,0 +1,34 @@
+const apiKey = "https://api.openweathermap.org/data/2.5/forecast?q={city name}&appid=d4d47f02cada0dd2afaf7aee439b9c40"
+const inputVal = input.val
+const url = "https://api.openweathermap.org/data/2.5/forecast?q={city name}&appid=d4d47f02cada0dd2afaf7aee439b9c40"
+
+// fetch(url)
+//   .then(response => response.json())
+//   .then(data => {
+//     console.log 
+//   })
+//   .catch(() => {
+//     msg.textContent = "Please search for a valid city ";
+//   });
+
+
+// function to add previous city names to previous cities ul and display 
+
+
+// funtion to display the city name and current time when searched 
+
+
+// function to apply the current temperature + wind + humidity 
+
+
+// function to list the next 5 days weather forcast
+        // functon to add in the selected picture icons in relation to that day 
+            // for loop, "if the temperature is between 0 - 20, 20-40 , 40-60, 60-80, 80-100 "
+            // or look to see if emoji is sent with the weather 
+            // ask john 
+
+
+
+
+
+

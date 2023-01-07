c1f14c6a38b4b51052eabb95a7929413d272f082
Mary Webby <MaryWebby@Marys-MacBook-Air-3.local>
2023-01-07 12:38:49 -0500

    new files

--- assets/script.js
+++ assets/script.js
@@ -9,16 +9,51 @@ searchButton.addEventListener("click", function (event) {
     console.log(cityName)
     event.preventDefault()
 
-    let url = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`
-        fetch(url)
-            .then(function (response) {
-                 return response.json()
-             }) 
-            .then (function (data) {
-                    console.log(data);
-             });
+    let url = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=imperial&appid=${apiKey}`
+    fetch(url)
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (data) {
+            console.log(data);
+            var cardArray = []
+            for (var i =0; i<data.list.length; i++){
+                var timeStamp = data.list[i].dt_txt.split(" ")[1]
+                if (timeStamp === "12:00:00") {
+                    cardArray.push(data.list[i])
+                }
+            }
+            displayForcast(cardArray)
+            var weather = data.list[0]
+            var currentData = { temp: weather.main.temp, wind: weather.wind.speed, humidity: weather.main.humidity, name:data.city.name}
+            displayCurrentWeather(currentData)
         });
+});
+function displayForcast (weatherArray) {
+    for (var i = 0 ; i < weatherArray.length ; i ++) {
+        var cardDiv = document.createElement("div")
+        cardDiv.setAttribute("class", "card")
+        cardDiv.setAttribute("style", "width:18rem")
+        var cardBody = document.createElement("div")
+        cardBody.setAttribute("class", "card-body")
+        cardDiv.appendChild(cardBody)
+        document.querySelector(".container-cards").appendChild(cardDiv)
+    }
+}
 
+
+function displayCurrentWeather (weatherobj){
+    document.querySelector(".container-info").innerHTML = ""
+    var pName = document.createElement("p")
+    pName.textContent = weatherobj.name
+    var pTemp = document.createElement("p")
+    pTemp.textContent = "Temperature: " + weatherobj.temp
+    var pWind = document.createElement("p")
+    pWind.textContent = "Wind: " + weatherobj.wind
+    var pHumidity = document.createElement("p")
+    pHumidity.textContent = "Humidity: " + weatherobj.humidity
+    document.querySelector(".container-info").append(pName, pTemp, pWind, pHumidity)
+}
 
 // data.list[0].main.temp
 
@@ -36,10 +71,10 @@ searchButton.addEventListener("click", function (event) {
 
 
 // function to list the next 5 days weather forcast
-        // functon to add in the selected picture icons in relation to that day 
+        // functon to add in the selected picture icons in relation to that day
             // for loop, "if the temperature is between 0 - 20, 20-40 , 40-60, 60-80, 80-100 "
-            // or look to see if emoji is sent with the weather 
-            // ask john 
+            // or look to see if emoji is sent with the weather
+            // ask john
 
 
 
